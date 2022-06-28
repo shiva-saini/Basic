82c5daec88748587ee670996af48a78413a6affc
shiva-saini <shivasainisiyana2211@gmail.com>
2022-06-28 15:03:12 +0530

    commites

--- app.js
+++ app.js
@@ -1,76 +1,151 @@
-const path = require("path");
+// const path = require("path");
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const errorController = require("./controllers/error");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const session = require("express-session");
+// const MongoDBStore = require("connect-mongodb-session")(session);
+// const errorController = require("./controllers/error");
 
-const User = require("./models/user");
+// const User = require("./models/user");
+// const csrf = require('csurf');
+// const MONGODB_URI =
+//   "mongodb+srv://shivasaini:adiAXQ073WWOwV7Z@shiva-oauth-test.gdqyj.mongodb.net/shop?retryWrites=true&w=majority";
+
+
+// const app = express();
+
+// const store = new MongoDBStore({
+//   uri: MONGODB_URI,
+//   collection: "session",
+// }); //this is a constructor
+
+// const csrfProtection = csrf();
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+// const authRoutes = require("./routes/auth");
+
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// //note session route should come before all other routes
+// app.use(
+//   session({ secret: "my secret", resave: false, saveUninitialized: false,store:store })
+// );
+
+// app.use(csrfProtection);
+
+
+
+// app.use((req,res,next)=>{
+//   if(!req.session.user){
+//     return next();
+//   }
+//   User.findById(req.session.user._id)
+//        .then(user => {
+//         req.user = user;
+//         next();
+//        })
+//        .catch(err=>{console.log(err)});
+// });
+
+// app.use((req,res,next) =>{
+//   req.locals.isAuthenticated = req.session.isLoggedIn;
+//   req.locals.csrfToken = req.csrfToken();
+//   next();
+// })
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+// app.use(authRoutes);
+// app.use(errorController.get404);
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(() => {
+//     app.listen(3000);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+
+
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const flash = require('connect-flash');
 const MONGODB_URI =
-  "mongodb+srv://shivasaini:adiAXQ073WWOwV7Z@shiva-oauth-test.gdqyj.mongodb.net/shop?retryWrites=true&w=majority";
-
+  'mongodb+srv://shivasaini:adiAXQ073WWOwV7Z@shiva-oauth-test.gdqyj.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
-
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: "session",
-}); //this is a constructor
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
-
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-//note session route should come before all other routes
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false,store:store })
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
+app.use(csrfProtection);
+app.use(flash());
 
-
-
-
-app.use((req,res,next)=>{
-  if(!req.session.user){
+app.use((req, res, next) => {
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
-       .then(user => {
-        req.user = user;
-        next();
-       })
-       .catch(err=>{console.log(err)});
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
 app.use(errorController.get404);
+
 mongoose
   .connect(MONGODB_URI)
-  .then(() => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "shiva",
-          email: "test@gamil.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
+  .then(result => {
     app.listen(3000);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
+
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,231 @@
-const User = require('../models/user')
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodeMailer");
+const crypto = require("crypto");
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  // let message = req.flash('error');
+  // let ermessage = null;
+  // // console.log(message);
+  // if(message.lentgh > 0){
+  //   ermessage = message[0];
+  // 'mrraghav9412@gmail.com'
+  // }else{
+  //   ermessage = null;
+  // }
+
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: req.flash("error"),
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+    errorMessage: req.flash("error"),
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  
-  User.findById("62a4828598bc674207948b4c")
-  .then(user => {
-    req.session.isLoggedIn = true;
-    req.session.user = user;
-    req.session.save(err => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password.");
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          req.flash("error", "Invalid email or password.");
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          const msg = {
+            from: "shivasainisiyana2211@gmail.com",
+            to: email,
+            subject: "testing mail",
+            text: "testing mail sent from my side(shiva saini)",
+          };
+          res.redirect("/login");
+          return nodemailer
+            .createTransport({
+              service: "gmail",
+              auth: {
+                user: "shivasainisiyana2211@gmail.com",
+                pass: "suzhzogpgjzykvsg",
+              },
+              port: 465,
+              host: "smtp.gmail.com",
+            })
+            .sendMail(msg, (err) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("sent");
+              }
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
       console.log(err);
-      res.redirect('/');
     });
-  })
-  .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err)=>{
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
+};
+
+exports.getReset = (req, res, next) => {
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    isAuthenticated: false,
+    errorMessage: req.flash("error"),
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.pseudoRandomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "Sorry , user with this email does not exist.");
+          return res.redirect("/reset");
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        const msg = {
+          from: "shivasainisiyana2211@gmail.com",
+          to: req.body.email,
+          subject: "Password reset",
+          html: `
+                 <p> You requested a password reset</p>
+                 <p> Click this <a href="http://localhost:3000/reset/${token}">Link</a> 
+                  to set a new password</p>
+                `,
+        };
+
+        return nodemailer
+          .createTransport({
+            service: "gmail",
+            auth: {
+              user: "shivasainisiyana2211@gmail.com",
+              pass: "suzhzogpgjzykvsg",
+            },
+            port: 465,
+            host: "smtp.gmail.com",
+          })
+          .sendMail(msg, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("sent");
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => console.log(err));
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        isAuthenticated: false,
+        errorMessage: req.flash("error"),
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword,12);
+    })
+    .then(hashedPassword =>{
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result =>{
+      res.redirect('/login');
+    })
+    .catch((err) => console.log(err));
 };
